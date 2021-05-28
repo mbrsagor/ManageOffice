120eaf0c977c01b78c84088d4e56463ccf668d3e
mbrsagor <mbrsagor@gmail.com>
2021-05-29 01:15:14 +0600

    Refactor: Refactor repo

--- tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import Application from 'super-rentals/app';
-import config from 'super-rentals/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
