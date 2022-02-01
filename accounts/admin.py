c399e885a55dceaefe1893d278990b10ca1daf4c
mbrsagor <mbrsagor@gmail.com>
2022-02-01 14:12:07 +0600

    New: user list item show in django dashbaord

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdminManager(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'pin', 'is_superuser']
+    list_display_links = ['username', ]
+
+
+admin.site.register(User, UserAdminManager)
