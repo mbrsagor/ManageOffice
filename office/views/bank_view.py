55f51a25cc8b41561866b41700b9d992caa6f331
mbrsagor <mbrsagor@gmail.com>
2023-01-25 22:21:24 +0600

    New: Offer rating API done.

--- office/views/bank_view.py
+++ office/views/bank_view.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
+from rest_framework import viewsets, permissions, views
 
 from office.models import Bank
 from office.serializers.bank_serailizer import BankSerializer
@@ -22,3 +23,24 @@ class BankViewSet(viewsets.ModelViewSet):
             "is_active": true
         }
     """
+
+
+# class OfferCrateView(views.View):
+#     def post(self, request, *args, **kwargs):
+#         access = self.request.user.role
+#         if access == enum_utils.allow_access_consumer:
+#             try:
+#                 serializer = offer_serializer.OfferRatingSerializer(data=request.data)
+#                 if serializer.is_valid(raise_exception=True):
+#                     serializer.save(consumer=self.request.user)
+#                     # Rating added point will add to consumer account
+#                     consumer = User.objects.get(id=self.request.user.id)
+#                     points = 5.00
+#                     consumer.points = F('points') + points
+#                     consumer.save()
+#                     return Response(response.prepare_success_response(messages.OFFER_SAVE), status=status.HTTP_200_OK)
+#                 return Response(response.prepare_error_response(serializer.errors), status=status.HTTP_404_NOT_FOUND)
+#             except Exception as ex:
+#                 return Response(response.prepare_error_response(str(ex)), status=status.HTTP_400_BAD_REQUEST)
+#         else:
+#             return Response(response.prepare_error_response(messages.PERMISSION_MSG))
