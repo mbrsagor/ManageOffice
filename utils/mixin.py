e995abbd5b8249401fae19c0c56f1244c0722512
mbrsagor <mbrsagor@gmail.com>
2023-02-02 23:22:53 +0600

    New: add mixin for basic utils.

--- utils/mixin.py
+++ utils/mixin.py
@@ -1,3 +1,20 @@
+import random
+import string
+from django.conf import settings
+
+
+# Base URL remove extra splash
+def base_url():
+    url = settings.SITE_URL
+    uri = url.replace('/', '')
+    return uri
+
+
+# When user will create device token will automatically create random 10 digit string
+letters = string.ascii_lowercase
+random_device_token = ''.join(random.choice(letters) for i in range(10))
+
+
 # Get average rating
 def average_rating(num):
     rating = 0
