782d191e924f29f775c66ee69bdca570cced8c9e
mbrsagor <mbrsagor@gmail.com>
2023-06-24 15:36:05 +0600

    New: order form listview design.

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'id', 'username', 'email', 'first_name', 'last_name', 'is_superuser', 'is_active'
+            'id', 'username', 'email', 'first_name',
+            'last_name', 'is_superuser', 'is_active'
         )
 
 
