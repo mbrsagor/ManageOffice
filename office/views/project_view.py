279baa637ece28f02ab94cf5b20fd60661dd39f2
mbrsagor <mbrsagor@gmail.com>
2023-09-06 08:01:12 +0600

    New: project API endpoint added notofication.

--- office/views/project_view.py
+++ office/views/project_view.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics, permissions, status
 from django_filters import rest_framework as filters
+from rest_framework.response import Response
 
 from office.models import Project, Client
-from office.serializers.project_serializer import ProjectSerializer
-from pagination.default_pagination import StandardResultsSetPagination
 from utils.employee_info import Evolution
+from office.serializers.project_serializer import ProjectSerializer
+from utils.notification_manager import send_notification_single_user
+from pagination.default_pagination import StandardResultsSetPagination
 
 
 class ProjectFilter(filters.FilterSet):
@@ -30,6 +32,20 @@ class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAdminUser]
     pagination_class = StandardResultsSetPagination
+
+    def create(self, request, *args, **kwargs):
+        # Static message and title
+        title = "Add your title"
+        message = "Your message added here or load sommewhere else"
+        try:
+            serializer = ProjectSerializer(data=self.request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                send_notification_single_user(self.request.user.device_token, title, message)
+                return Response('Project added successfully', status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            return Response(str(ex), status=status.HTTP_200_OK)
 
 
 class ProjectFilterView(generics.ListAPIView):
